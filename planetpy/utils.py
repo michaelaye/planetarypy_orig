f529da9adfe9486629e91ab3b1a67126921d54aa
K.-Michael Aye <kmichael.aye@gmail.com>
2016-05-06 13:28:02 -0600

    first commit

---
+++ planetpy/utils.py
@@ -0,0 +1,34 @@
+import datetime as dt
+import gdal
+
+nasa_date_format = '%Y-%j'
+nasa_dt_format = nasa_date_format + 'T%H:%M:%S'
+standard_date_format = '%Y-%m-%d'
+standard_dt_format = standard_date_format + 'T%H:%M:%S'
+
+
+def nasa_date_to_iso(datestr):
+    date = dt.datetime.strptime(datestr, nasa_date_format)
+    return date.isoformat()
+
+
+def iso_to_nasa_date(datestr):
+    date = dt.datetime.strptime(datestr, standard_date_format)
+    return date.strftime(nasa_date_format)
+
+
+def nasa_datetime_to_iso(dtimestr):
+    time = dt.datetime.strptime(dtimestr, nasa_dt_format)
+    return time.isoformat()
+
+
+def iso_to_nasa_datetime(dtimestr):
+    date = dt.datetime.strptime(dtimestr, standard_dt_format)
+    return date.strftime(nasa_dt_format)
+
+
+def get_center_coords(imgpath):
+    ds = gdal.Open(str(imgpath))
+    xmean = ds.RasterXSize//2
+    ymean = ds.RasterYSize//2
+    return xmean, ymean
