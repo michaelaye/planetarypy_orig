e74f0eb916ae42686b2bb7c33c585aed3ec5ba5f
K.-Michael Aye <kmichael.aye@gmail.com>
2020-04-11 06:34:01 +0000

    pull in this function on top level

--- planetarypy/__init__.py
+++ planetarypy/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 
-from ._config import config
+from ._config import config  # noqa
+from ._config import set_database_path  # noqa
 
 __author__ = "K.-Michael Aye"
 __email__ = "kmichael.aye@gmail.com"
